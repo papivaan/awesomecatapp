20bda61f800d44e3d62e25d06e4a0046e9af46a2
Renne Hirsimäki <git@rennehir.fi>
2016-12-17 17:15:34 +0200

    Created a method for sending a message when pressed Send button.

--- app/src/main/java/com/example/awesomecatapp/MainActivity.java
+++ app/src/main/java/com/example/awesomecatapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.awesomecatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when user clicks the "Send" button
+     * @param view View that was clicked
+     */
+    public void sendMessage(View view) {
+        // Code here...
     }
 }

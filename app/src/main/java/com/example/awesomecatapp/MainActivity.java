e7c8d6767a48e093d1f885d5c682bd6e6ae66933
Renne Hirsimäki <git@rennehir.fi>
2016-12-17 17:28:43 +0200

    Created DisplayMessageActivity that shows the text written into the text box. Helppoo ja kivaa!

--- app/src/main/java/com/example/awesomecatapp/MainActivity.java
+++ app/src/main/java/com/example/awesomecatapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.awesomecatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.awesomecatapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
      * @param view View that was clicked
      */
     public void sendMessage(View view) {
-        // Code here...
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }

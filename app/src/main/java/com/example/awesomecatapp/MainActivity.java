2e2bd5bc317cf136a73ce1d755c565722b6b838c
Renne Hirsimäki <git@rennehir.fi>
2016-12-18 02:59:46 +0200

    no message

--- app/src/main/java/com/example/awesomecatapp/MainActivity.java
+++ app/src/main/java/com/example/awesomecatapp/MainActivity.java
@@ -1,36 +1,10 @@
 package com.example.awesomecatapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -51,10 +25,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                
+                //TODO: API-kutsu varmaan tähän
+                String catFact = "Not initialized yet...";
+
+
 
                 //Set Text on button click via this function.
-                newText.setText("Text Change successfully");
+                newText.setText(catFact);
 
             }
         });

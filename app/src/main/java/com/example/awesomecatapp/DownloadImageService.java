6f9dff277b9d0f74ec20da66152f0746c25873dc
Renne Hirsimäki <git@rennehir.fi>
2016-12-21 19:00:33 +0200

    Päivitetty kuvanlatauspalvelua. Ei varmaan vielä toimi täysin, pittee vielä ihmetellä…

--- app/src/main/java/com/example/awesomecatapp/DownloadImageService.java
+++ app/src/main/java/com/example/awesomecatapp/DownloadImageService.java
@@ -3,7 +3,21 @@ package com.example.awesomecatapp;
 import android.app.IntentService;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.test.espresso.core.deps.guava.io.CharStreams;
 import android.util.Log;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 
 public class DownloadImageService extends IntentService {
@@ -11,10 +25,10 @@ public class DownloadImageService extends IntentService {
     public static final String ACTION_DOWNLOAD = "com.example.awesomecatapp.action.DOWNLOAD";
     //public static final String ACTION_BAZ = "com.example.dara.myapplication.action.BAZ";
 
-    public static final String EXTRA_URL = "com.example.awesomecatapp.extra.URL";
-    public static final String EXTRA_MESSAGE = "com.example.awesomecatapp.extra.message";
+    public static final String EXTRA_APIURL = "com.example.awesomecatapp.extra.APIURL";
+    //public static final String EXTRA_MESSAGE = "com.example.awesomecatapp.extra.message";
 
-    public static final Bitmap IMAGE = null;
+    public static Bitmap img = null;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -34,11 +48,34 @@ public class DownloadImageService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_DOWNLOAD.equals(action)) {
-                final String url = intent.getStringExtra(EXTRA_URL);
-                Log.e("Service", url);
-                downloadImage(url);
+                final String apiUrl = intent.getStringExtra(EXTRA_APIURL);
+                Log.e("Service", apiUrl);
+                String imgUrl = downloadXml(apiUrl);
+                downloadImage(imgUrl);
             }
         }
+    }
+
+
+    /**
+     * Connects to the cat pic API and parses the image url from the XML
+     * @param apiUrl URL of the API to be used
+     * @return URL of the image
+     */
+    private String downloadXml(String apiUrl) {
+
+        String xmlString = null;
+
+        try {
+            InputStream in = new java.net.URL(apiUrl).openStream();
+            xmlString = CharStreams.toString(new InputStreamReader(in, "UTF-8"));
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        String imgUrl = parseImgUrl(xmlString);
+        return imgUrl;
     }
 
 
@@ -47,4 +84,60 @@ public class DownloadImageService extends IntentService {
 
 
     }
+
+
+    /**
+     * Parses the image URL out of the XML string
+     * @param xmlString XML string that contains URL to the image
+     * @return URL to the image
+     */
+    public String parseImgUrl(String xmlString) {
+
+        String imageUrl = "";
+
+        try {
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder;
+            Document doc = null;
+            try {
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(new InputSource(new StringReader(xmlString)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            NodeList nList = null;
+            if (doc != null) {
+                doc.getDocumentElement().normalize();
+                nList = doc.getElementsByTagName("image");
+            }
+
+
+
+            System.out.println("----------------------------");
+
+            if (nList != null) {
+                for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                    Node nNode = nList.item(temp);
+
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                        Element eElement = (Element) nNode;
+
+                        imageUrl = eElement.getElementsByTagName("url").item(0).getTextContent();
+                        System.out.println("Image URL: " + imageUrl);
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return imageUrl;
+    }
+
 }
